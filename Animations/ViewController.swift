5d544c2141336696551fa433c62dc7c641f4466e
Carrington <Manyuchi>
2023-07-29 19:55:05 +0200

    animations

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -1,19 +1,37 @@
-//
 //  ViewController.swift
 //  Animations
-//
 //  Created by DA MAC M1 157 on 2023/07/29.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myView: UIView = {
+        let view = UIView()
+        view.frame  = CGRect(x: 0, y: 0, width: 100, height: 100)
+        //view.center = view.center
+        view.backgroundColor = .black
+        view.layer.cornerRadius = view.frame.size.width / 2
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myView)
+        myView.center = self.view.center
     }
 
-
+    // called when screen is tapped
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 2.5) {
+            //self.myView.frame.origin.y = self.myView.frame.origin.y / 2
+            self.myView.frame.size = CGSize(width: 300, height: 300)
+            self.myView.layer.cornerRadius = self.myView.frame.size.width / 2
+            self.myView.center = self.view.center
+            self.myView.alpha = 0.1
+            
+            
+        }
+    }
 }
 
